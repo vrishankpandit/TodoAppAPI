1ec405a7be3e969bdbe9e1badb5c1a56b69e5733
Vishu <vrishankpandit1344@gmail.com>
2025-02-15 17:13:41 +0530

    add retrieveTodoById and deleteTodoById

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/TodoResource.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/TodoResource.java
@@ -2,6 +2,8 @@ package com.in28minutes.rest.webservices.restfulwebservices.todo;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +20,17 @@ public class TodoResource {
 	@GetMapping(path = "/users/{username}/todos")
 	public List<Todo> retrieveTodos(@PathVariable String username) {
 		return todoService.findByUsername(username);
+	}
+	
+	@GetMapping(path = "/users/{username}/todos/{id}")
+	public Todo retrieveTodoById(@PathVariable String username,@PathVariable int id) {
+		return todoService.findById(id);
+	}
+	
+	@DeleteMapping(path = "/users/{username}/todos/{id}")
+	public ResponseEntity<Void> deleteTodo(@PathVariable String username,@PathVariable int id) {
+		todoService.deleteById(id);
+		return ResponseEntity.noContent().build();	//will return a status of no content back
 	}
 	
 }
