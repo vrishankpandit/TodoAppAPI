3ec324631b9eac15c64c77da4e8a72c359541c2b
Vishu <vrishankpandit1344@gmail.com>
2025-02-18 18:57:57 +0530

    allow Options request and add a test url (/basicauth)

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/basic/BasicAuthenticationSecurityConfiguration.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/basic/BasicAuthenticationSecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.in28minutes.rest.webservices.restfulwebservices.basic;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -13,7 +14,11 @@ public class BasicAuthenticationSecurityConfiguration {
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		
-		http.authorizeHttpRequests(auth->auth.anyRequest().authenticated()); //enables authentication on evry api call
+		http.authorizeHttpRequests(auth->auth
+										.anyRequest().authenticated()
+										.antMatchers(HttpMethod.OPTIONS,"/**").permitAll() //anables access to preflight request
+									
+									); //enables authentication on evry api call
 		
 		http.httpBasic(Customizer.withDefaults());//enables basic auth so user can enter username,password
 		
